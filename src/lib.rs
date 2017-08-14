09ba64b9d98fac595a3f965c6440b668fe20850a
Michael Lazear <lazear@scripps.edu>
2017-08-13 23:03:09 -0700

    Fixed bug in test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+//! Simple dependency-free threadpool based on code from 
+//! The Rust Programming Language Book (Http Server example)
+
 use std::thread;
-use std::sync::{Arc, Mutex, MutexGuard, mpsc};
+use std::sync::{Arc, Mutex,mpsc};
 
 trait FnBox {
     fn call(self: Box<Self>);
@@ -18,7 +21,7 @@ enum Message {
     Terminate,
 }
 
-pub struct Worker {
+struct Worker {
     thread: Option<thread::JoinHandle<()>>,
 }
 
@@ -28,32 +31,53 @@ pub struct ThreadPool {
 }
 
 impl ThreadPool {
+    /// Create a new ThreadPool with a `count` threads
+    /// running in the background
+    /// 
+    /// # Example
+    ///
+    /// ```rust norun
+    /// let pool = threadpool::ThreadPool::new(4);
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// Will panic if `count` equals zero
     pub fn new(count: usize) -> ThreadPool {
         assert!(count > 0);
 
         let (sender, receiver) = mpsc::channel();
-
-        //let (status_s, status_r) = mpsc::channel::<bool>();
-
         let receiver = Arc::new(Mutex::new(receiver));
-        let mut workers = Vec::with_capacity(count);
 
-        for id in 0..count {
-            workers.push(Worker::new(receiver.clone()));
-        }
 
         ThreadPool {
-            workers: workers,
+            workers: (0..count).map(|_| Worker::new(receiver.clone())).collect(),
             sender: sender,
         }
     }
 
+    /// Execute a task on the ThreadPool
+    /// 
+    /// # Example
+    ///
+    /// ```rust
+    /// // Create a ThreadPool with 4 threads running
+    /// let pool = threadpool::ThreadPool::new(4);
+    /// for i in 0..16 {
+    ///     pool.execute(move || println!("threadpool!"))
+    /// }
+    /// ```
     pub fn execute<F: FnOnce() + Send + 'static>(&self, func: F) {
         self.sender.send(Message::Work(Box::new(func))).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
+    /// Stop the ThreadPool
+    ///
+    /// All currently running tasks will be completed first
+    ///
+    /// Automatically called when the ThreadPool falls out of scope
     fn drop(&mut self) {
         for _ in &mut self.workers {
             self.sender.send(Message::Terminate).unwrap();
@@ -103,24 +127,22 @@ mod tests {
         let count = Arc::new(Mutex::new(0));
 
         for i in 0..20 {
-            {
-                let mut data = count.clone();
-        
-                pool.execute(move || { 
-                    //::std::thread::sleep_ms(100); 
-                    println!("{}", i);
-                    let mut data = data.lock().unwrap();
-                    println!("{:?}", data);
-                    *data += 1;
-                    //*c.lock().unwrap() += 1;
-                    //*c += 1;
-                    //*c += 1;
-                });
-            }
+            let mut data = count.clone();
 
+            // Share a mutex to mutable data, increment the value by 1     
+            pool.execute(move || { 
+                let lock = data.lock();
+                match lock {
+                    Ok(mut data) => {
+                        *data += 1;
+                    },
+                    Err(_) => println!("locked"),
+                };
+            });
         }
-
-        assert_eq!(*count.try_lock().unwrap(), 20);
-        println!("All jobs submitted");
+        // wait for all jobs to finish
+        drop(pool);
+        // Make sure that all threads completed
+        assert_eq!(*count.lock().unwrap(), 20);
     }
 }
